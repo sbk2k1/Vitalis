c95ec2643524c3f3810297cad9170424a3d9a9ab
Saptarshi <bhattacharyasaptarshi2001@gmail.com>
2023-07-24 23:01:38 +0530

    Finished

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { isUser } from "../../api/index";
 import "./home.css";
 
-export default function Home() {
+export default function Landing() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -15,9 +15,9 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="home">
+    <div className="landing">
       <header className="header">
-        <h1>Server and SQL Health Monitoring</h1>
+        <h1 style={{ marginLeft: 15 }}>Vitalis</h1>
         <nav>
           {!loggedIn && (
             <Link to="/login">
@@ -25,30 +25,26 @@ export default function Home() {
             </Link>
           )}
           {loggedIn && (
-            <div style={{height: 10, display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Link to="/workspaces/api" style={{marginRight: 15}}>
-              <button>Workspaces</button>
-            </Link>
-            <Link to="/about">
-              <button>About</button>
-            </Link>
-          </div>
+            <div>
+              <Link to="/workspaces/api" style={{ marginRight: 15 }}>
+                <button>Workspaces</button>
+              </Link>
+            </div>
           )}
         </nav>
       </header>
-      <section className="banner">
-        <h3>Welcome to our Monitoring Website!</h3>
-        <p>Keep track of your servers and SQL databases in real-time.</p>
+      <section className="hero">
+        <div className="hero-content">
+          <h3>Welcome to Vitalis!</h3>
+          <div className="hero-right">
+          <p>Keep track of your API endpoints in real-time.</p>
+          <Link to="/about">
+            <button className="cta-button">Learn More</button>
+          </Link>
+          </div>
+        </div>
       </section>
-      <section className="features">
-        <h3>Features</h3>
-        <ul>
-          <li>Real-time server health monitoring</li>
-          <li>SQL database performance metrics</li>
-          <li>Alerts and notifications</li>
-          <li>Data visualization and analytics</li>
-        </ul>
-      </section>
+      
     </div>
   );
 }
